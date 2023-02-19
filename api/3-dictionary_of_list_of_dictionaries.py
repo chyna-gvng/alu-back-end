f8aec4f097ac01f0b453d4cba0a04fc59838c128
Walter <w.angoye@alustudent.com>
2023-02-19 17:53:23 +0300

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,38 +6,18 @@ import json
 import requests
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
     """
-        request user info & store in dictionary
+        export to JSON
     """
 
-    request_users = requests.get('https://jsonplaceholder.typicode.com/users/')
-    users = request_users.json()
-    list_of_users = {}
-    for i in range(1, len(users) + 1):
-        request_employee = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}/'.format(i))
-        user = json.loads(request_employee.text)
-        username = user.get("username")
-
-        request_todos = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}/todos'.format(i))
-        tasks = {}
-        user_todos = request_todos.json()
-        for dictionary in user_todos:
-            tasks.update(
-                {dictionary.get("title"): dictionary.get("completed")})
-
-        task_list = []
-        for k, v in tasks.items():
-            task_list.append({
-                "username": username,
-                "task": k,
-                "completed": v,
-            })
-        list_of_users[i] = task_list
-        """
-            export to JSON
-        """
-
-    with open('todo_all_employees.json', mode='w') as file:
-        json.dump(list_of_users, file)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
